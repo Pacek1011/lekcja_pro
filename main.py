6dbd22bd22e77bc084111c0a163415291ab72258
Pacek1011 <145258859+Pacek1011@users.noreply.github.com>
2023-09-17 12:00:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            "ROFL": "odpowiedź na żart",
+            "SHEESH":  "lekka dezaprobata",
+            "CREEPY":  "straszny, złowieszczy",
+            "AGGRO":  "stać się agresywnym/zły",
+            }
+            
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
 
+
+if word in meme_dict.keys():
+    print(word, ":", meme_dict[word])
+else:
+    print("nie mamy takiego słowa w słowniku.")

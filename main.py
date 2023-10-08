06097c5645011ebad98c7c523c27af2ee7bcf531
Pacek1011 <145258859+Pacek1011@users.noreply.github.com>
2023-10-08 12:00:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-meme_dict = {
-            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
-            "LOL": "Częsta reakcja na coś zabawnego",
-            "ROFL": "odpowiedź na żart",
-            "SHEESH":  "lekka dezaprobata",
-            "CREEPY":  "straszny, złowieszczy",
-            "AGGRO":  "stać się agresywnym/zły",
-            }
-            
-word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+import discord
+from settings import settings
+from botlogic import *
+from discord.ext import commands
 
+# Zmienna intencje przechowuje uprawnienia bota
+intents = discord.Intents.default()
+# Włączanie uprawnienia do czytania wiadomości
+intents.message_content = True
+# Tworzenie bota w zmiennej klienta i przekazanie mu uprawnień
+# client = discord.Client(intents=intents)
 
-if word in meme_dict.keys():
-    print(word, ":", meme_dict[word])
-else:
-    print("nie mamy takiego słowa w słowniku.")
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot {bot.user}!')
+    
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+        
+bot.run(settings["TOKEN"])
